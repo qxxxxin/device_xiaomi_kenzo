a2cbcda2911988af24b4c2b41e6075ab67b8c2eb
Gitesh Dubal <48854049+Gitskill99@users.noreply.github.com>
2019-04-02 13:37:53 +0530

    Update aosp_kenzo for CAF version

--- aosp_kenzo.mk
+++ aosp_kenzo.mk
@@ -17,7 +17,7 @@ $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit some common PixelExperience stuff.
 TARGET_GAPPS_ARCH := arm64
